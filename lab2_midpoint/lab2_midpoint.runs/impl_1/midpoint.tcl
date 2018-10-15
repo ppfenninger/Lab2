01d6badcbd71c2c78bb93d4c06868084caadc8c5
ppfenninger <paige.pfenninger@students.olin.edu>
2018-10-14 20:02:04 -0400

    wrote a test file

--- lab2_midpoint/lab2_midpoint.runs/impl_1/midpoint.tcl
+++ lab2_midpoint/lab2_midpoint.runs/impl_1/midpoint.tcl
@@ -61,92 +61,23 @@ proc step_failed { step } {
 }
 
 
-start_step init_design
-set ACTIVE_STEP init_design
+start_step write_bitstream
+set ACTIVE_STEP write_bitstream
 set rc [catch {
-  create_msg_db init_design.pb
-  create_project -in_memory -part xc7z010clg400-1
-  set_property board_part digilentinc.com:zybo:part0:1.0 [current_project]
-  set_property design_mode GateLvl [current_fileset]
-  set_param project.singleFileAddWarning.threshold 0
+  create_msg_db write_bitstream.pb
+  open_checkpoint midpoint_routed.dcp
   set_property webtalk.parent_dir /home/comparch/Lab2/lab2_midpoint/lab2_midpoint.cache/wt [current_project]
-  set_property parent.project_path /home/comparch/Lab2/lab2_midpoint/lab2_midpoint.xpr [current_project]
-  set_property ip_output_repo /home/comparch/Lab2/lab2_midpoint/lab2_midpoint.cache/ip [current_project]
-  set_property ip_cache_permissions {read write} [current_project]
-  add_files -quiet /home/comparch/Lab2/lab2_midpoint/lab2_midpoint.runs/synth_1/midpoint.dcp
-  read_xdc /home/comparch/Lab2/lab2_midpoint/lab2_midpoint.srcs/constrs_1/imports/comparch/ZYBO_Master.xdc
-  link_design -top midpoint -part xc7z010clg400-1
-  close_msg_db -file init_design.pb
+  catch { write_mem_info -force midpoint.mmi }
+  write_bitstream -force midpoint.bit 
+  catch {write_debug_probes -quiet -force midpoint}
+  catch {file copy -force midpoint.ltx debug_nets.ltx}
+  close_msg_db -file write_bitstream.pb
 } RESULT]
 if {$rc} {
-  step_failed init_design
+  step_failed write_bitstream
   return -code error $RESULT
 } else {
-  end_step init_design
-  unset ACTIVE_STEP 
-}
-
-start_step opt_design
-set ACTIVE_STEP opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  opt_design 
-  write_checkpoint -force midpoint_opt.dcp
-  create_report "impl_1_opt_report_drc_0" "report_drc -file midpoint_drc_opted.rpt -pb midpoint_drc_opted.pb -rpx midpoint_drc_opted.rpx"
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-  unset ACTIVE_STEP 
-}
-
-start_step place_design
-set ACTIVE_STEP place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
-    implement_debug_core 
-  } 
-  place_design 
-  write_checkpoint -force midpoint_placed.dcp
-  create_report "impl_1_place_report_io_0" "report_io -file midpoint_io_placed.rpt"
-  create_report "impl_1_place_report_utilization_0" "report_utilization -file midpoint_utilization_placed.rpt -pb midpoint_utilization_placed.pb"
-  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file midpoint_control_sets_placed.rpt"
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-  unset ACTIVE_STEP 
-}
-
-start_step route_design
-set ACTIVE_STEP route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force midpoint_routed.dcp
-  create_report "impl_1_route_report_drc_0" "report_drc -file midpoint_drc_routed.rpt -pb midpoint_drc_routed.pb -rpx midpoint_drc_routed.rpx"
-  create_report "impl_1_route_report_methodology_0" "report_methodology -file midpoint_methodology_drc_routed.rpt -pb midpoint_methodology_drc_routed.pb -rpx midpoint_methodology_drc_routed.rpx"
-  create_report "impl_1_route_report_power_0" "report_power -file midpoint_power_routed.rpt -pb midpoint_power_summary_routed.pb -rpx midpoint_power_routed.rpx"
-  create_report "impl_1_route_report_route_status_0" "report_route_status -file midpoint_route_status.rpt -pb midpoint_route_status.pb"
-  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file midpoint_timing_summary_routed.rpt -pb midpoint_timing_summary_routed.pb -rpx midpoint_timing_summary_routed.rpx -warn_on_violation "
-  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file midpoint_incremental_reuse_routed.rpt"
-  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file midpoint_clock_utilization_routed.rpt"
-  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file midpoint_bus_skew_routed.rpt -pb midpoint_bus_skew_routed.pb -rpx midpoint_bus_skew_routed.rpx"
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  write_checkpoint -force midpoint_routed_error.dcp
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
+  end_step write_bitstream
   unset ACTIVE_STEP 
 }
 
